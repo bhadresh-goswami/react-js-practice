984a4eb52665d71680406cef2c5bb80aa765bb1c
bhadresh-goswami <gosai09@gmail.com>
2020-04-30 15:10:57 -0400

    upload first practice
    hello world

---
+++ intro-app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Hello from './components/hello';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Hello/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
